cf09225c7ac3854c21608258369936292400c4ea
BenMcMenamin <112865063+BenMcMenamin@users.noreply.github.com>
2023-08-29 22:14:39 +0100

    Add files via upload

---
+++ HW.py
@@ -0,0 +1,76 @@
+QUESTION 1:
+a. INVALID - Should be no use of "." working one would be "studentNumber"
+b. VALID
+c. INVALID - Must begin with either "_" or a letter
+d. VALID
+e. VALID
+f. VALID
+g. INVALID - Must begin with either "_" or a letter
+h. VALID
+i. VALID
+j. VALID
+k. INVALID - No space allowed
+l. VALID
+m. VALID
+n. INVALID - No "+" allowed
+o. VALID
+
+QUESTION 2:
+1. Missing ")"
+2. P should not be capital and missing ")"
+3. missing quotation at end
+4. missing both brackets
+5. missing both brackets and space
+6. missing both qoutations
+7. NO ERROR
+8. NO ERROR
+9. NO ERROR
+
+QUESTION 3:
+1. def main():
+    name = input("Enter your name: ")
+    address = input("Enter your address: ")
+    
+    print("\nName:", name)
+    print("Address:", address)
+
+if __name__ == "__main__":
+    main()
+    
+2. print("######    ######    ######")
+print("    ##    ##        ##")
+print("######    ######    ######")
+print("    ##    ##  ##        ##")
+print("######    ######    ######")
+
+QUESTION 4:
+1. A       B
+C
+
+2. C:\Users\johndoe\Documents\myfile.txt
+
+QUESTION 5:
+1. personName = "Alex"
+favouriteColour = "red"
+
+print(f"Hi {personName} - your favourite colour is {favouriteColour}")
+print(f"Goodbye {personName}")
+
+2.
+
+3. a. The variable days Left is intialized to 167
+b. The value 11.27 is stored in the variable called rate
+c. The value Alex is assigned to the variable name
+d.
+e. The value entered by the user is stored in the variavle pwd
+f.
+
+QUESTION 6:
+personName = "Alex"  # This is a string
+print(type(personName))
+
+Age = 23  # This is an integer
+print(type(Age))
+
+Height = 1.8  # This is a float
+print(type(Height))
